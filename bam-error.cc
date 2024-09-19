5920b11b43e7ae7e882111b306e444bfbda17c74
Tell-dev <tellbaza@gmail.com>
2024-09-19 17:14:08 +0700

    Delete bam-error.cc

--- bam-error.cc
+++
@@ -1,4 +0,0 @@
-aborted. cond="file->Fail()", msg="Unable to Open core_traffic/core-0-0.pcap for mode 16", +0.000000000s -1 file=/home/ddos/ns-allinone-3.42/ns-3.42/src/network/helper/trace-helper.cc, line=59
-NS_FATAL, terminating
-terminate called without an active exception
-Command 'build/scratch/ns3.42-topology3-default' died with <Signals.SIGABRT: 6>.
